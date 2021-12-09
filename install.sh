7d8bde0a03c1ac1036aff0a98fe50ef06beed4d3
Youssef1042005 <youssefbotroussghaly@gmail.com>
2021-12-09 14:44:58 +0000

    first commit

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+apt-get update -y
+apt-get upgrade -y
+apt-get install nodejs -y
+apt-get install libwebp -y
+apt-get install mc -y
+apt-get install ffmpeg -y
+apt-get install wget -y
+apt-get install tesseract -y
+apt-get install nmap -y
+apt update -y
+apt upgrade -y
+pkg update && pkg upgrade  -y
+pkg install hot -y
+pkg install bash -y
+pkg install libwebp -y
+pkg install git -y
+pkg install nodejs -y 
+pkg install ffmpeg -y 
+pkg install wget -y
+pkg install imagemagick -y
+cd
+rm -rf bot-doge-olduser
+ls
+rm -rf bot-doge-virtual
+ls
+cd
+git clone https://github.com/jetushack1/bot-doge-virtual
+git clone https://github.com/jetushack1/bot-doge-olduser
+cd bot-doge-virtual
+ls
+rm -rf session.json
+npm install
+npm start
+Scan qr code within 15seconds
